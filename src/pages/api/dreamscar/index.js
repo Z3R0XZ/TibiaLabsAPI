317f5a8d1f5684a0aee0fdaa1aac6225cfa6323f
Lucas Hames <me@wed.tf>
2022-06-30 21:17:38 -0300

    Update index.js

--- src/pages/api/dreamscar/index.js
+++ src/pages/api/dreamscar/index.js
@@ -7,9 +7,14 @@ const cors = initMiddleware(
     methods: ["GET"],
   })
 );
-
 export default async function handler(req, res) {
   await cors(req, res);
   const bossName = await DreamScarBoss();
-  res.send(`Today's Dream Scar Boss should be ${bossName} on most worlds.`);
+  const time = new Date().toISOString();
+  const seconds = new Date().getSeconds();
+
+  res.setHeader("Cache-Control", "s-maxage=10, stale-while-revalidate=59");
+  res.send(
+    `Today's Dream Scar Boss should be ${bossName} on most worlds. ${seconds}`
+  );
 }
