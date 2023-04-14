48074eade86100d9d3ff7a9726ac5c0437adc810
Z3R0XZ <beto_tidus@hotmail.com>
2023-04-14 01:01:43 -0600

    Create [name].js
    New Char seach func

---
+++ src/pages/api/chars/[name].js
@@ -0,0 +1,26 @@
+import Cors from "cors";
+import initMiddleware from "../../../lib/init-middleware";
+import { CharacterInfo } from "../../../services/TibiaData";
+import { VocationStats } from "../../../services/CharacterDetailedInfo";
+
+const cors = initMiddleware(
+  Cors({
+    methods: ["GET"],
+  })
+);
+
+export default async function handler(req, res) {
+  await cors(req, res);
+  const { name } = req.query;
+  const info = await CharacterInfo(name);
+  const infoVoc = await VocationStats(info.level, info.vocation);
+  res.setHeader("Cache-Control", "s-maxage=600, stale-while-revalidate=60");
+
+  if (info.vocation != "") {
+    res.send(
+      `${info.name} [${info.level}] - Voc: ${info.vocation} - Mundo: ${info.world}`
+    );
+  } else {
+    res.send(`El Personaje ${name} no existe.`);
+  }
+}
